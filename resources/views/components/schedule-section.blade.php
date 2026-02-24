66b80001023f4ba87036c7f83e81ed65b16b97f4
John Congdon <john@phparch.com>
2026-02-24 13:50:23 -0800

    Update schedule-section.blade.php

--- resources/views/components/schedule-section.blade.php
+++ resources/views/components/schedule-section.blade.php
@@ -5,6 +5,6 @@
             <h2 class="text-3xl md:text-4xl font-display font-bold mb-4">Schedule</h2>
         </div>
 
-        <script type="text/javascript" src="https://sessionize.com/api/v2/7u1xfdke/view/GridSmart"></script>
+        <script type="text/javascript" src="https://sessionize.com/api/v2/7u1xfdke/view/GridSmart" defer></script>
     </div>
 </section>
